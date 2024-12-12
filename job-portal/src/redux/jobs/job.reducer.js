5bea9a530e1505349c2e9e59967e7231b9998210
GUDDU ALI <gudduali93@yahoo.com>
2024-12-12 15:39:47 +0530

    Create job.reducer.js

---
+++ job-portal/src/redux/jobs/job.reducer.js
@@ -0,0 +1,54 @@
+import {
+  ERROR_IN_GET_JOB,
+  GET_ALL_JOBS,
+  JOB_LOADING,
+  SEARCH,
+  ADD_TO_BOOKMARK
+} from "./job.action";
+
+const initialJobState = {
+  AllJobs: [],
+  Search: [],
+  isLoading: false,
+  isError: null,
+};
+
+export const Reducer = (state = initialJobState, { type, payload }) => {
+  switch (type) {
+    case JOB_LOADING:
+      return {
+        ...state,
+        isLoading: true,
+        isError: null,
+      };
+    case ERROR_IN_GET_JOB:
+      return {
+        ...state,
+        isLoading: false,
+        isError: payload,
+      };
+    case GET_ALL_JOBS:
+      return {
+        ...state,
+        isLoading: false,
+        isError: null,
+        AllJobs: payload,
+      };
+    case ADD_TO_BOOKMARK:
+      return {
+        ...state,
+        isLoading: false,
+        isError: null,
+        AllJobs: [...state.AllJobs, payload],
+      };
+    case SEARCH:
+      return {
+        ...state,
+        isLoading: false,
+        isError: null,
+        Search: [...state.Search, ...payload],
+      };
+    default:
+      return state;
+  }
+};
